175fe2852ea37a92249c3c78d598fcb47a666abf
JohnVemula1998 <johnvemula7771998@gmail.com>
2022-02-05 18:52:04 +0530

    Updates

---
+++ precache-manifest.67892a5839b92c335381b5820a84db29.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e6625c86d53f4c2ee75",
+    "url": "/firstcomponent/static/css/main.30298bc2.chunk.css"
+  },
+  {
+    "revision": "2e6625c86d53f4c2ee75",
+    "url": "/firstcomponent/static/js/main.77e42c38.chunk.js"
+  },
+  {
+    "revision": "caa89a30008fefd31266",
+    "url": "/firstcomponent/static/js/runtime~main.60787a7d.js"
+  },
+  {
+    "revision": "b7117a5d63189cdc881d",
+    "url": "/firstcomponent/static/js/2.492b1f6f.chunk.js"
+  },
+  {
+    "revision": "8e657167247dce61a612b350e492a033",
+    "url": "/firstcomponent/index.html"
+  }
+];
